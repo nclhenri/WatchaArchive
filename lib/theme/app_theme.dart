d6d1e862c74b42c70a3d09716d156cd2fe50d2a0
nclhenri <nicolashenrisilva@outlook.com>
2026-07-02 22:14:07 -0300

    adicionando fontes

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,1 +1,14 @@
 // Cores, fontes, espaçamentos vindos do Figma
+
+import 'package:flutter/material.dart'
+
+class AppColors {
+    static const pretoFonte = Color(#000000);
+    static const background = Color(#E6E6E6)
+}
+
+final appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: AppColors.pretoFonte),
+  scaffoldBackgroundColor: AppColors.background,
+  fontFamily: 'Inria Serif',
+);
