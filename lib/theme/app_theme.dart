a88de3a73236ae481c1a45311b9ec48c961d5748
nclhenri <nicolashenrisilva@outlook.com>
2026-07-03 20:16:06 -0300

    Configurando e Adicionando Fontes

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,14 +1,16 @@
 // Cores, fontes, espaçamentos vindos do Figma
 
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
 class AppColors {
-    static const pretoFonte = Color(#000000);
-    static const background = Color(#E6E6E6)
+    static const pretoFonte = Color(0xFF000000);
+    static const background = Color(0xFFE6E6E6);
+    static const amareloExclamacao = Color(0xFFF2B51B);
+    static const corBotao = Color(0xFFD9D9D9);
 }
 
 final appTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: AppColors.pretoFonte),
+  colorScheme: ColorScheme.fromSeed(seedColor: AppColors.background),
   scaffoldBackgroundColor: AppColors.background,
   fontFamily: 'Inria Serif',
 );
